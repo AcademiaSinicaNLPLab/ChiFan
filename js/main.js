e8e704173835eeaa990567f1b04a8089c008291c
maxis <maxis1718@gmail.com>
2014-07-30 18:11:34 +0800

    Add restaurant data

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
+var candidates = ["KLG","手工面","孫媽媽","八方","老家前門","老家後門","XO面","國王","鐵板燒","三國","滷味（重抽）","燒臘","中餐廳(重抽)","seven","偽八方","日本料理","MOS","麥噹噹(Maxis :D)","casa (Dr.Virk)","subway","真肯德基","異國noodle","絕食","西瓜麵店"];
+
 $(document).ready(function () {
 
     $('input.focus').focus();
 
-    inputEvents();
+    // $.getJSON('data/restaurants.json', function(candidates){
+    console.log(candidates);
+    bindInputEvents();
+    // });
 });
 
+// will pop all the elements in the origin list
+function shuffle(list)
+{
+    var shuffled = [];
 
-function inputEvents()
+    while(list.length)
+    {
+        var idx = Math.round(Math.random()*(list.length-1));
+        if(idx > -1)
+        {
+            var ele = list.splice(idx, 1)[0];
+            shuffled.push(ele);
+        }
+    }
+    list = shuffled;
+    return shuffled;
+}
+
+function choice(list, num)
+{
+    // var idx = Math.round(Math.random()*(list.length-1));
+    return list[num%list.length];
+}
+
+
+function bindInputEvents()
 {
     var target = $('.button').find('span');
+    
     $('input[type="text"]').keydown(function(e){
         if( e.which == 32)
         {
-            target.click();
+            // target.click();
         }
         else if( e.which >= 48 && e.which <= 57 || 
                 e.which >= 37 && e.which <= 40  || 
@@ -29,14 +59,51 @@ function inputEvents()
     });
 
     var input = $('input[type="text"]');
+
     target.click(function(){
 
         var raw_input = $.trim(input.val());
 
-        if(!isNaN(raw_input))
+        var num = parseInt(raw_input);
+
+        if(isNaN(num))
         {
-            var num = parseInt(raw_input);
-            $('.result').text(num);
+            // clear current
+            $('.result').slideUp(100);
+
+        }else
+        {
+            candidates = shuffle(candidates);
+            var res = choice(candidates, num);
+            $('.result').slideUp(100, function(){
+                $('.result').text(res);
+            }).slideDown(250);
+            
         }
+
+        // if(!isNaN(raw_input))
+        // {
+        //     var num = 
+
+            
+        //     // console.log(num);
+
+        //     
+
+        //     
+
+        //     
+        // }
+
+//         $('.result').slideUp(100, function(){
+//         
+
+// else
+//             {
+
+//             }
+//         }).slideDown(250);
+
+
     });    
 }
